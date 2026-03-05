726559e85cdbdf5673c4e11e6989f27c83c1125e
Noah Thiering <noah.thiering@gmail.com>
2026-03-05 03:22:42 +0100

    fix: update SHA256 checksums

--- Formula/sshconfig-lint.rb
+++ Formula/sshconfig-lint.rb
@@ -7,20 +7,20 @@ class SshconfigLint < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/Noah4ever/sshconfig-lint/releases/download/v#{version}/sshconfig-lint-macos-arm64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "1769c6ae9befec7ddf7173637e44102a744f7a9d98bdbac3a68ebd2dd64a4950"
     else
       url "https://github.com/Noah4ever/sshconfig-lint/releases/download/v#{version}/sshconfig-lint-macos-x86_64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "16f6bac929072f75215e00e55e319f41bd25d705543c2ddbbbb04b49ea44e8de"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/Noah4ever/sshconfig-lint/releases/download/v#{version}/sshconfig-lint-linux-arm64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "08db3193fae246216d8cad0c88cff0faeefaa5a8987e227d0441fbefce82bb1e"
     else
       url "https://github.com/Noah4ever/sshconfig-lint/releases/download/v#{version}/sshconfig-lint-linux-x86_64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "d4f39629ad57382807cabb768cbae9bc37939e3dc23e2f218934d28e4e9dcdd3"
     end
   end
 
@@ -41,6 +41,6 @@ class SshconfigLint < Formula
   end
 
   test do
-    assert_match "sshconfig-lint", shell_output("#{bin}/sshconfig-lint --version")
+    assert_match version.to_s, shell_output("#{bin}/sshconfig-lint --version")
   end
 end
